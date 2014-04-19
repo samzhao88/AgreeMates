0d351195ed7cd16837d15daa2f27e398378b28c3
Aengus McMillin <aengusmcmillin@gmail.com>
2014-04-19 15:54:14 -0700

    Added Vagrantfile and puppet configuration Automatically creates an ubuntu virtual machine with the usual mean stack components. Doesn't deal with PostgreSQL yet because I'm not yet sure how we are going to create that database.

---
+++ installation/shell/install_agreemates.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+AGREEMATES_DIR=/AgreeMates
+
+if [ ! -d "$AGREEMATES_DIR" ]; then
+	cd /
+	git clone https://github.com/AgreeMates/AgreeMates.git
+	cd $AGREEMATES_DIR
+	npm install
+fi
+
