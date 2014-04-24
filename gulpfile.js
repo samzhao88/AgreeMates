eda52db6a364162c260e34ea4799a18e233cff9e
Aengus McMillin <aengusmcmillin@gmail.com>
2014-04-23 21:05:42 -0700

    Basic structure and more testing stuff

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var istanbul = require('gulp-istanbul');
+var mocha = require('gulp-mocha');
 
 // Lint Task
 gulp.task('lint', function() {
@@ -27,6 +29,25 @@ gulp.task('scripts', function() {
 // Watch Files For Changes
 gulp.task('watch', function() {
 	gulp.watch('js/*.js', ['lint', 'scripts']);
+});
+
+// Set up the file coverage
+gulp.task('cover', function (cb) {
+	gulp.src(['server/**/*.js', '!server/test/**'])
+		.pipe(istanbul())
+		.on('end', cb);
+});
+
+function handleError(err) {
+	console.log(err.toString());
+	this.emit('end');
+}
+
+// Run tests and output reports
+gulp.task('test', function () {
+	gulp.src('./server/test/**/*.js')
+		.pipe(mocha({ reporter: 'list' }))
+		.on("error", handleError);
 });
 
 // Default Task
