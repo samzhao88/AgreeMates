6c5874c85a14f60369f5f6bcf527c915f67c00aa
Dennis Ding <dennisd7@u.washington.edu>
2014-04-26 20:44:06 -0700

    added navbar.js controller

---
+++ public/app/controllers/navbar.js
@@ -0,0 +1,8 @@
+var navList = angular.module('navList', []);
+
+navList.controller('navCtrl', ['$scope', '$location', function ($scope, $location) {
+    $scope.navClass = function (page) {
+        var currentRoute = $location.path().substring(1) || 'home';
+        return page === currentRoute ? 'active' : '';
+    };        
+}]);
