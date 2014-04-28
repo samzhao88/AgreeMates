6fac666c64649773236e6f7b224cdb6a46d2778e
Amit Burstein <amit.burstein@gmail.com>
2014-04-27 19:39:01 -0700

    Set up Express middleware, move server configs to config.js

--- server/app.js
+++ server/app.js
@@ -1,16 +1,30 @@
 var express = require('express');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var compress = require('compression');
 var bookshelf  = require('bookshelf');
+var config = require('./config');
 
 var app = express();
 
 //this should only be called once in the application -> backend put that somewhere in the configs pls :)
-var bookshelf.pg = Bookshelf.initialize({
+var db = bookshelf.initialize({
   client: 'pg',
   connection: {
-    host     : '127.0.0.1',
-    user     : 'your_database_user',
-    password : 'your_database_password',
-    database : 'myapp_test',
+    host     : config.db.host,
+    user     : config.db.user,
+    password : config.db.password,
+    database : config.db.database,
     charset  : 'utf8'
   }
+});
+
+app.use(logger());
+app.use(compress());
+app.use(bodyParser());
+app.use(cookieParser());
+
+app.listen(config.port, function() {
+  console.log('Server running on port ' + config.port);
 });
