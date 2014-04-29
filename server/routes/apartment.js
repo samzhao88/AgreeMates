542a05cff781889125bd652189872b67693029ed
Jordan Heier <heierj@uw.edu>
2014-04-29 09:48:02 -0700

    Set up apartment router

--- server/routes/apartment.js
+++ server/routes/apartment.js
@@ -1,5 +1,29 @@
 var apartment = function(app) {
 
+  app.get('/apartment/add', function(req, res) {
+    res.end();
+  });
+
+  app.get('/apartment/edit/:apt', function(req, res) {
+    res.end();
+  });
+
+  app.get('/apartment/edit', function(req, res) {
+    res.end();
+  });
+
+  app.get('/apartment/delete', function(req, res) {
+    res.end();
+  });
+
+  app.get('/apartment/info', function(req, res) {
+    res.end();
+  });
+
+  app.get('/apartment/add/roommate', function(req, res) {
+    res.end();
+  });
+
 };
 
 module.exports = apartment;
