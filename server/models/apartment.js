ca65bd7c9b066002be165fabc8dc2a5523005417
William McNamara <mcnawi11@uw.edu>
2014-04-29 13:49:50 -0700

    added aparment model

--- server/models/apartment.js
+++ server/models/apartment.js
@@ -1,14 +1,30 @@
 /* 
- * This is the model representation for a single .
+ * This is the model representation for a single apartment
  */
 
 var Bookshelf = db;
 
-var Model = require("./").model;
+var BoardModel = require("./board").model;
+var SupplyModel = require("./supply").model;
+var ChoreModel = require("./chore").model;
+var BillModel = require("./bill").model;
+var UserModel = require("./user").model;
 
 exports.model = Bookshelf.Model.extend({
-	tableName: "",
-	: function() {
-		return this.belongsToOne(Model);
+	tableName: "apartments",
+	board: function() {
+		return this.hasOne(BoardModel);
+	},
+	supplies: function() {
+		return this.hasMany(SupplyModel);
+	},
+	ownedChores: function() {
+		return this.hasMany(ChoreModel);
+	},
+	ownedBills: function() {
+		return this.hasMany(BillModel);
+	},
+	users: function() {
+		return this.belongsToMany(UserModel);
 	},
 });
