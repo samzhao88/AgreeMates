ae0d786fd27659869cec4fc5dd63d627929dad3b
Nick Evans <nick.romero.evans@gmail.com>
2014-05-02 14:46:26 -0700

    Commented first half of the routes (apartment-chores

--- server/routes/calendar.js
+++ server/routes/calendar.js
@@ -1,5 +1,9 @@
+// server/routes/calendar.js
+// Calendar Routes
+
 var calendar = function(app) {
 
+ // Shows the apartment calendar
   app.get('/calendar/show', function(req, res) {
     res.json({title: 'Calendar'});
   });
