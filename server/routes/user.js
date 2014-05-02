d4baac7e8b07b47d83ee0950e67f61c03b09938f
Jordan Heier <heierj@attu1.cs.washington.edu>
2014-05-02 14:43:06 -0700

    Added comments to some routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,21 +1,29 @@
+// Located in server/routes/user.js
+// Defines all the routes for users
+
 var user = function(app) {
 
+  // Adds a user to the database
   app.get('/user/add', function(req, res) {
     res.end();
   });
 
+  // Gets the users current information so they can edit it
   app.get('/user/edit/:user', function(req, res) {
     res.end();
   });
 
+  // Updates the users information changes
   app.post('/user/edit', function(req, res) {
     res.end();
   });
 
+  // Gets the users current notification preferences so they can edit it
   app.get('/user/edit/preferences/:user', function(req, res) {
     res.end();
   });
 
+  // Updates the users preferences changes
   app.post('/user/edit/preferences', function(req, res) {
     res.end();
   });
