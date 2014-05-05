912552131aa7fa812c6589273933b3c523d69a08
Aengus McMillin <aengusmcmillin@gmail.com>
2014-05-05 12:57:57 -0700

    Modularized the application and updated some naming to match normal conventions

--- public/app/navbar/navbar.js
+++ public/app/navbar/navbar.js
@@ -1,7 +1,9 @@
 'use strict';
 
+angular.module('main.nav', []);
+
 // Angular controller for navigation bar
-angular.module('Nav').controller('navbarController',
+angular.module('main.nav').controller('NavCtrl',
                                  function ($scope, $location) {
 
   $scope.isActive = function(viewLocation) {
---
+++ public/app/profile/profile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('main.profile', []);
+
+// Angular controller for user profile
+angular.module('main.profile').controller('ProfileCtrl', function
+                                          ($scope, $http) {
+
+  $http.get('/profile').
+    success(function(data) {
+      $scope.title = data.title;
+  });
+
+});
