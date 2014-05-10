1e79508c3478f22800b91d941a14bd88eea8d4a0
Dennis Ding <dennisd7@u.washington.edu>
2014-05-09 18:37:19 -0700

    some stuff

--- public/app/components/apartment/apartment.js
+++ public/app/components/apartment/apartment.js
@@ -7,7 +7,7 @@ angular.module('main.apartment').controller('AptAddCtrl',
   function ($http, $scope) {
 
     $scope.add = function(apartment) {
-      $http.post('/apartment/add', apartment)
+      $http.post('/apartment', apartment)
         .success(function(data, status, headers, config) {
           if(data.result=="success"){
             window.location.href = "./";
--- public/app/components/chores/chores.js
+++ public/app/components/chores/chores.js
@@ -5,6 +5,21 @@ angular.module('main.chores', []);
 // Angular controller for chores
 angular.module('main.chores').controller('ChoresCtrl',
   function ($scope, $http) {
+  	$scope.add = function(apartment) {
+      $http.post('/chores', apartment)
+        .success(function(data, status, headers, config) {
+          if(data.result=="success"){
+            window.location.href = "./";
+          }
+        })
+        .error(function(data, status, headers, config) {
+          console.log(status, headers, config);
+      });
+    };
+
+
+
+
 
     $http.get('/chores').
       success(function(data) {
