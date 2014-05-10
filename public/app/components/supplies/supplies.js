69fb4f6c29d8e70435937162bba9497b46e780c1
Lukas Bischofberger <lukasbi@student.ethz.ch>
2014-05-10 13:09:27 -0700

    supplies

--- public/app/components/supplies/supplies.js
+++ public/app/components/supplies/supplies.js
@@ -1,14 +1,66 @@
 'use strict';
 
-angular.module('main.supplies', []);
+angular.module('main.supplies', ['ui.bootstrap']);
 
 // Angular controller for supplies
 angular.module('main.supplies').controller('SuppliesCtrl',
   function ($scope, $http) {
 
-    $http.get('/supplies').
+  	$scope.hideAddBox = true;
+
+    var dummy = [{id: 1, name: 'toilet paper emtpy', status: 0},
+  				{id: 2, name: 'toilet paper running low', status: 1},
+  				{id: 3, name: 'toilet paper fully stocked', status: 2}];
+
+  	//get apll supplies
+    $http.get('/').
       success(function(data) {
-        $scope.title = data.title;
-    });
+        console.log(dummy);
+        data = dummy;
+        $scope.supplies = data;
+
+      });
+
+      $scope.showAdd = function(){
+      	$scope.hideAddBox = $scope.hideAddBox === false ? true: false;
+      };
+
+      //add a supply
+      $scope.addSupply = function(){
+      	$http.post('/supplies/',$scope.newSupply).
+	      success(function(data) {
+	        if(data.error){
+	        	console.log(data.error);
+	        } else {
+	        	$scope.supplies.push($scope.newSupply);
+	        }
+	      });
+      };
+
+      //update a supply
+      $scope.updateSupply = function(index){
+
+      	$http.put('/supplies/', $scope.supplies[index]).
+	      success(function(data) {
+	        if(data.error){
+	        	console.log(data.error);
+	        	//should change radio button back?
+	        } else {
+	        	//do nothing because radio has already changed
+	        }
+	      });
+      };
+
+      //delete a supply
+      $scope.deleteSupply = function(id, index){
+      	$http.delete('/supplies/'+id).
+	      success(function(data) {
+	        if(data.error){
+	        	console.log(data.error);
+	        } else {
+	        	$scope.supplies.splice(index, 1);
+	        }
+	      });
+      };
 
 });
