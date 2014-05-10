6d0fd6ea86f913af3e493b6a312ef783162433e5
Dennis Ding <dennisd7@u.washington.edu>
2014-05-10 15:05:55 -0700

    add chore function available

--- public/app/components/chores/chores.js
+++ public/app/components/chores/chores.js
@@ -5,11 +5,50 @@ angular.module('main.chores', []);
 // Angular controller for chores
 angular.module('main.chores').controller('ChoresCtrl',
   function ($scope, $http) {
-  	$scope.add = function(apartment) {
-      $http.post('/chores', apartment)
-        .success(function(data, status, headers, config) {
+  	//global variable to hold users in apartment
+  	$scope.users = [];
+
+	$http.get('/user/:user').
+      	success(function(data) { 
+
+      	//get users in apartment
+      	$scope.users = data;
+
+      	//dummy users
+      	$scope.users = [ {name: "alice", isChecked: false } , {name: "bob", isChecked: false} 
+      	, {name: "cameron", isChecked: false}];
+
+        console.log("grabbing users");
+        console.log($scope.users);
+    });	
+
+  	//adding a chore
+  	$scope.add = function(chore) {
+  		chore.responsible = [];
+
+  		//add responsible persons
+  		angular.forEach($scope.users, function(s){
+			if (s.isChecked){chore.responsible.push(s.name)
+			}
+		});
+  		//chore.interval = 10; 
+  		//console.log(chores);
+
+  		console.log(chore)
+
+      	$http.post('/chores', chore)
+        .success(function(data) {
+
+
           if(data.result=="success"){
-            window.location.href = "./";
+            console.log("success");
+          }
+          else
+          {
+          	console.log("error");
+          	console.log(chore.name);
+          	console.log(chore.interval);
+          	console.log(chore.responsible);
           }
         })
         .error(function(data, status, headers, config) {
@@ -17,7 +56,9 @@ angular.module('main.chores').controller('ChoresCtrl',
       });
     };
 
-
+    $scope.cancel = function() {
+    	console.log("cancel arggg");
+    };
 
 
 
@@ -25,5 +66,8 @@ angular.module('main.chores').controller('ChoresCtrl',
       success(function(data) {
         $scope.title = data.title;
       });
+
+    //grabbing all users in apartment
+
 
 });
