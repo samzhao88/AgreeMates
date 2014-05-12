8e6f78afc46ed017b09b79566197cba76a8f2241
Lukas Bischofberger <lukasbi@student.ethz.ch>
2014-05-12 15:14:47 -0700

    edit supplies, error msgs

--- public/app/components/supplies/supplies.js
+++ public/app/components/supplies/supplies.js
@@ -48,12 +48,19 @@ angular.module('main.supplies').controller('SuppliesCtrl',
         });
     };
 
+    $scope.editSupply = function(ind){
+      console.log(ind);
+      $scope.supplies[ind].edit = true;
+    }
+
     //update a supply
     $scope.updateSupply = function(index){
 
     	$http.put('/supplies/'+$scope.supplies[index].id, $scope.supplies[index]).
 	      success(function(data) {
 	        //do nothing because radio has already changed
+          console.log(data);
+          $scope.supplies[index].edit = false;
 	      }).
         error(function(data, status, headers, config){
           console.log(data);
@@ -68,7 +75,7 @@ angular.module('main.supplies').controller('SuppliesCtrl',
           $scope.successmsg = "Supply "+ $scope.supplies[index].name+" successfully deleted!";
 	        $scope.success = true;
           $timeout(function(){$scope.success=false;},1000);
-            
+          $scope.supplies.splice(index, 1);
 	      }).
         error(function(data, status, headers, config){
           console.log(data);
