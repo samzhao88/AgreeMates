77596cc24afd42c30e1ffd72272a29b5b6eaeba8
Lukas Bischofberger <lukasbi@student.ethz.ch>
2014-05-12 15:14:47 -0700

    edit supplies, error msgs

--- public/app/components/supplies/supplies.js
+++ public/app/components/supplies/supplies.js
@@ -45,8 +45,15 @@ angular.module('main.supplies').controller('SuppliesCtrl',
         error(function(data, status, headers, config){
           console.log(data);
           $scope.errormsg = data.error;
+          $scope.error = true;
+          $timeout(function(){$scope.error=false;},1000);
         });
     };
+
+    $scope.editSupply = function(ind){
+      console.log(ind);
+      $scope.supplies[ind].edit = true;
+    }
 
     //update a supply
     $scope.updateSupply = function(index){
@@ -54,10 +61,14 @@ angular.module('main.supplies').controller('SuppliesCtrl',
     	$http.put('/supplies/'+$scope.supplies[index].id, $scope.supplies[index]).
 	      success(function(data) {
 	        //do nothing because radio has already changed
+          console.log(data);
+          $scope.supplies[index].edit = false;
 	      }).
         error(function(data, status, headers, config){
           console.log(data);
           $scope.errormsg = data.error;
+          $scope.error = true;
+          $timeout(function(){$scope.error=false;},1000);
         });
     };
 
@@ -68,11 +79,13 @@ angular.module('main.supplies').controller('SuppliesCtrl',
           $scope.successmsg = "Supply "+ $scope.supplies[index].name+" successfully deleted!";
 	        $scope.success = true;
           $timeout(function(){$scope.success=false;},1000);
-            
+          $scope.supplies.splice(index, 1);
 	      }).
         error(function(data, status, headers, config){
           console.log(data);
           $scope.errormsg = data.error;
+          $scope.error = true;
+          $timeout(function(){$scope.error=false;},1000);
         });
     };
 
