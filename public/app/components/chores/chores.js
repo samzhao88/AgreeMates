92235995a4bba1dedbc4d278e8eb3ae0d4afa22a
Dennis Ding <dennisd7@u.washington.edu>
2014-05-12 20:06:47 -0700

    fixed some chores bugs

--- public/app/components/chores/chores.js
+++ public/app/components/chores/chores.js
@@ -8,7 +8,7 @@ angular.module('main.chores').controller('ChoresCtrl',
   	//holds all chores for apartment
     $scope.chores = [];
 
-
+    $scope.chore = {name: ''};
     //get chores
     // $http.get('/chores/').
     // 	success(function(data) {
@@ -18,17 +18,17 @@ angular.module('main.chores').controller('ChoresCtrl',
     $http.get('/chores').
     	success(function(data) {
         	$scope.chores = data.chores;
-        	console.log(data);
-        	console.log("hello");
+        	//console.log(data);
+        	//console.log("hello");
 			for( var i = 0; i < $scope.chores.length; i++ )
       		{
       			$scope.chores[i].isSelected = false;
-      			console.log($scope.chores[i].name);
+      			//console.log($scope.chores[i].name);
       		}
 
       	}).
       	error(function(data, status, headers, config){
-        	console.log(data);
+        	//console.log(data);
 
     });
 
@@ -44,7 +44,7 @@ angular.module('main.chores').controller('ChoresCtrl',
       	$scope.users = data;
 
       	//dummy users
-      	$scope.users = [ {name: "alice", isChecked: true } , {name: "bob", isChecked: false} 
+      	$scope.users = [ {name: "alice", isChecked: false, id: 12} , {name: "bob", isChecked: false, id: 12} 
       	, {name: "cameron", isChecked: false}];
 
       	// console.log($scope.users[0].name);
@@ -69,21 +69,31 @@ angular.module('main.chores').controller('ChoresCtrl',
 
 
   	//adding a chore
-  	$scope.add = function(chore) {
-  		chore.responsible = [];
-
+  	$scope.addChore = function() {
+  		//chore.user_id = [];
+ 	var chore = angular.copy($scope.chore);
   		//add responsible persons
-  		angular.forEach($scope.users, function(s){
-			if (s.isChecked){chore.responsible.push(s.name)
-			}
-		});
+  // 		angular.forEach($scope.users, function(s){
+		// 	if (s.isChecked){chore.user_id.push(s.user_id)
+		// 		console.log(chore);
+		// 	}
+		// });
   		//chore.interval = 10; 
 
   		//console.log(chore);
 
+  		chore.createdate = "10-2-14";
+  		chore.userid = 5;
+
+  // 		var myDate= chore.duedate;
+		// myDate=myDate.split("-");
+		// var newDate=myDate[1]+"/"+myDate[0]+"/"+myDate[2];
+		// chore.duedate = new Date(newDate).getTime();
+
+		// console.log(chore.duedate);
+
       	$http.post('/chores', chore)
         .success(function(data) {
-
 
           if(data.result=="success"){
             console.log("success");
@@ -91,20 +101,60 @@ angular.module('main.chores').controller('ChoresCtrl',
           else
           {
           	console.log("error");
-          	console.log(chore)
+          	console.log(chore);
           	console.log(chore.name);
           	console.log(chore.interval);
-          	console.log(chore.responsible);
+          	console.log(chore.user_id);
+          	console.log(chore.duedate);
           }
         })
         .error(function(data, status, headers, config) {
           console.log(status, headers, config);
-      });
+      	});
+      	$scope.cancel;
     };
+
+    //edit chore
+    $scope.editChore = function(index) {
+    	var chore = $scope.chores[index];
+    	$scope.chore = angular.copy(chore);
+    	console.log($scope.chore);
+    	console.log($scope.chore.users);
+    	$scope.chore.users = [].concat($scope.chore.users);
+    	$scope.chore.users = $scope.chore.users.map(function(any){return {name: any, isChecked: true }});
+
+
+    	console.log($scope.chore.users);	
+
+      	console.log($scope.chore);
+
+      	$http.put('/supplies/'+$scope.chore[index], $scope.chore).
+  	    success(function(data) {
+  	    //console.log("hello");    //do nothing because radio has already changed
+  	    }).
+        error(function(data, status, headers, config){
+        console.log(data);
+        console.log($scope.chore);
+        });
+    	$scope.cancel;
+    };
+
+    $scope.deleteChore = function(index)
+    {
+    	$scope.chores.splice(index, 1);
+    	$http.delete('/chores/').
+  	    success(function(data) {
+  	    $scope.chores.splice(index, 1);
+  	    }).
+        error(function(data, status, headers, config){
+        console.log(data);
+        });
+    }
 
     $scope.cancel = function() {
-    	console.log("cancel arggg");
+    	console.log($scope.chore.name);
+    	$scope.chore.name = '';
+    	console.log("reset");
     };
-
 
 });
--- server/routes/chores.js
+++ server/routes/chores.js
@@ -1,40 +1,150 @@
 // Chore routes
 
 'use strict';
+var choreModel = require('../models/chore').model;
+var choreCollection = require('../models/chore').collection;
 
 var chores = function(app) {
 
+  //Get all chores for an apartment
   app.get('/chores', function(req, res) {
-    res.json({chores: [{name: "dishes", interval: 7, users: "alice" }, {name: "garbage", interval: 1, users: "bob"}] });
+    res.json({chores: [{name: "dishes", interval: 7, users: [{"alice", "bob"] }], {name: "garbage", interval: 1, users: "bob"}] });
 
+    var apartmentId = req.user.attributes.apartment_id;
+
+    new choreCollection({apartment_id: apartmentId}).fetch().then(function(model) {
+    var chores = [];
+
+    for(var i = 0; i < model.length; i++){
+        var chore = model.models[i].attributes;
+
+        chores.push({
+            id: chore.id,
+            name: chore.name,
+            createdate: chore.createdate,
+            duedate: chore.duedate,
+            interval: chore.interval,
+            completed: chore.completed,
+            reocurring_id: chore.reocurring_id,
+            user_id: chore.user_id,
+            apartment_id: chore.apartment_id
+        });
+    }
+
+    res.json({chores: chores});
+    }).otherwise(function(){
+        res.json(503,{error: 'Database error.'});
+    });
   });
 
   // Process chore form and adds to database
   app.post('/chores', function(req, res) {
+  console.log(req.body);
+  
+  //console.log(req);
 
-
-
-
-
-
-    res.end();
+  var input = req.body;
+  
+  if(!(input)){
+    input = {};
+    input.name = req.body.name;
+    input.duedate = req.body.duedate;
+    input.interval = req.body.interval;
+    input.user_id = req.user.attributes.id;
+  }
+  var apartmentId = req.user.attributes.apartment_id;
+  var createDate = new Date;
+  
+  if(!isValidName(input.name)){
+    res.json(400, {error: 'Invalid chore name.'});
+    return;
+  }
+  
+  console.log(req.body);
+    new choreModel({apartment_id: apartmentId,
+                name: input.name,
+                duedate: input.name,
+                createdate: createDate,
+                user_id: input.user_id,
+                interval: input.interval,
+                })
+                .save()
+                .then(function(model){
+                var chore = model.attributes;
+                res.json(chore);
+                }).otherwise(function(){
+                res.json(503,{error: 'Database error.'});
+            });
   });
 
   // Get the chore information
   app.get('/chores/:chore', function(req, res) {
-    res.end();
+    
+    // var apartmentId = req.user.attributes.apartment_id;
+    // var choreId = req.params.chore;
+    // if (!isValidId(choreId)) {
+    //   res.json(400, {error: 'Invalid supply ID.'});
+    //   return;
+    // }
+
+    // new choreModel({apartment_id: apartmentId, id: choreId})
+    //     .fetch().then(function(model){
+    //         var chore = model.attributes;
+    //         res.json(chore);
+    //     }).otherwise(function(){
+    //         res.json(503,{error: 'Database error.'});
+    //     });
   });
 
   // Update the chore
   app.put('/chores/:chore', function(req, res) {
-    res.end();
+    var apartmentId = req.user.attributes.apartment_id;
+    var choreId = req.params.chore;
+    var name = req.body.name;
+    //var dueDate: req.body.date;
   });
 
   // Remove chore from database
   app.delete('/chores/:chore', function(req, res) {
-    res.end();
-  });
+    var apartmentId = req.user.attributes.apartment_id;
 
+    var input  = JSON.parse(req.body);
+
+    console.log(req);
+
+    if(!(input)){
+        var input = {};
+        input.id = req.params.id;
+    }
+
+    if (!isValidId(input.id)) {
+      res.json(400, {error: 'Invalid supply ID.'});
+      return;
+    }
+
+    new choreModel({id: input.id, apartment_id: apartmentId})
+        .destry()
+        .then(function(){
+            res.send(200);
+        }).otherwise(function() {
+            res.json(503, {error: 'Database error.'});
+        });
+  });
+  
+  //Checks if a chore name is valid
+  function isValidName(name) {
+    return name !== undefined && name !== null && name !== '';
+  }
+
+  // Checks if a chore ID is valid
+  function isValidId(id) {
+    return isInt(id) && id > 0;
+  }
+  
+  function isInt(value) {
+    /* jshint eqeqeq: false */
+    return !isNaN(value) && parseInt(value) == value;
+  }
 };
 
 module.exports = chores;
