a49c0d4bf63fd22744075f7ff74fd126c3650d03
NickRomeroEvans <nick.romero.evans@gmail.com>
2014-05-14 14:35:23 -0700

    Get chore 'works'

--- public/app/components/chores/chores.js
+++ public/app/components/chores/chores.js
@@ -4,11 +4,147 @@ angular.module('main.chores', []);
 
 // Angular controller for chores
 angular.module('main.chores').controller('ChoresCtrl',
-  function ($http, $scope) {
 
+function ($scope, $http) {
+  	//holds all chores for apartment
+    $scope.chores = [];
+
+    $scope.chore = {name: ''};
+
+    //get chores
     $http.get('/chores').
-      success(function(data) {
-        $scope.title = data.title;
-      });
+    	success(function(data) {
+        	$scope.chores = data.chores;
+        	//console.log(data);
+        	//console.log("hello");
+			//for( var i = 0; i < $scope.chores.length; i++ )
+      		//{
+      		//	$scope.chores[i].isSelected = false;
+      			//console.log($scope.chores[i].name);
+      		//}
+
+      	}).
+      	error(function(data, status, headers, config){
+        	//console.log(data);
+    });
+
+
+
+  	//global variable to hold users in apartment
+  	$scope.users = [];
+
+	$http.get('/user/:user').
+      	success(function(data) { 
+
+      	//get users in apartment
+      	$scope.users = data;
+
+      	//dummy users
+      	$scope.users = [ {name: "alice", isChecked: false, id: 12} , {name: "bob", isChecked: false, id: 12} 
+      	, {name: "cameron", isChecked: false}];
+
+      	// console.log($scope.users[0].name);
+    });	
+
+
+
+  	//adding a chore
+  	$scope.addChore = function() {
+  	
+   	var chore = angular.copy($scope.chore);
+
+    chore.roommates = [{"id": 123} , {"id": 142}, {"id":1231}];
+    chore.interval = 7;
+    chore.duedate = 11/12/2000;
+    chore.name = "hello";
+    //chore.roomates[0].name = "name";
+    console.log(chore);
+
+      	$http.post('/chores/', chore)
+        .success(function(data) {
+
+          if(data.result=="success"){
+            console.log("success");
+          }
+          else
+          {
+          	console.log("error");
+          	console.log(chore);
+          	console.log(chore.name);
+          	console.log(chore.interval);
+          	console.log(chore.user_id);
+          	console.log(chore.duedate);
+          }
+        })
+        .error(function(data, status, headers, config) {
+          console.log(status, headers, config);
+      	});
+      	$scope.cancel;
+    };
+
+    //edit chore
+    $scope.editChore = function(index) {
+    	console.log($scope.chore);
+    	console.log($scope.chore.users);
+    	//$scope.chore.users = [].concat($scope.chore.users);
+    	//$scope.chore.users = $scope.chore.users.map(function(any){return {name: any, isChecked: true }});
+
+
+      for( var i = 0; i < $scope.chores.length; i++ )
+      {
+      if (!$scope.chore.users[i].isChecked)
+      {
+          $scope.chore.users[i].splice(index, 1);
+      }
+      //console.log($scope.chores[i].name);
+      }
+
+
+    	console.log($scope.chore.users);	
+
+      	console.log($scope.chore);
+
+      	$http.put('/supplies/'+$scope.chore[index], $scope.chore).
+  	    success(function(data) {
+  	    //console.log("hello");    //do nothing because radio has already changed
+  	    }).
+        error(function(data, status, headers, config){
+        console.log(data);
+        console.log($scope.chore);
+        });
+    	$scope.cancel;
+    };
+
+    //deletes chore
+    $scope.deleteChore = function(index)
+    {
+    	$scope.chores.splice(index, 1);
+    	$http.delete('/chores/').
+  	    success(function(data) {
+  	    $scope.chores.splice(index, 1);
+  	    }).
+        error(function(data, status, headers, config){
+        console.log(data);
+        });
+    }
+
+    $scope.cancel = function() {
+    	console.log($scope.chore.name);
+    	$scope.chore.name = '';
+    	console.log("reset");
+    };
+
+    //sets the edit chore menu to the selected chore
+    $scope.setChore = function(index){
+    var chore = angular.copy($scope.chores[index]);
+    chore.users = [].concat(chore.users);
+    chore.users = chore.users.map(function(any){return {name: any, isChecked: true }});
+    //$scope.chore.users = [].concat($scope.chore.users);
+    //$scope.chore.users = $scope.chore.users.map(function(any){return {name: any, isChecked: true }});
+    console.log(chore);
+    $scope.chore = chore;
+    
+    console.log($scope.chore);
+    };
 
 });
--- server/routes/chores.js
+++ server/routes/chores.js
@@ -5,8 +5,7 @@ var ChoreModel = require('../models/chore').model;
 var ChoreCollection = require('../models/chore').collection;
 var UserChoreModel = require('../models/users_chores').model;
 var UserChoreCollection = require('../models/users_chores').collection;
-var async = require("async");
-
+var Bookshelf = require('bookshelf'); 
 var chores = function(app) {
 
   //Get all chores for an apartment
@@ -18,118 +17,80 @@ var chores = function(app) {
   
     var apartmentId = req.user.attributes.apartment_id;
 	
-	new ChoreCollection({apartment_id: apartmentId})
-	.fetch()
-	.then(function(model) {
-		var chores = [];
-		
-		for(var i = 0; i < model.length; i++){
-			var chore = model.models[i].attributes;
+	Bookshelf.DB.knex('chores')
+		.join('users_chores', 'chores.id', '=', 'users_chores.chore_id')
+		.join('users', 'users_chores.user_id', '=', 'users.id')
+		.where('chores.apartment_id', '=', apartmentId)
+		.select( 'chores.interval','chores.createdate', 
+				'chores.duedate', 'users.first_name',
+				'chores.name','chores.reocurring_id',
+				'users_chores.user_id', 'users_chores.chore_id',
+				'users_chores.order_index', 'chores.completed')
+		.orderBy('users_chores.chore_id')
+		.then(function(rows){
+			var chores = [];
+			var users_chores = [];
+			if(rows.length === 0){
+				res.json({chores: chores});
+				return;
+			}
 			
-			var userChores = [];
-			/*var userChoreModels = model.models[i].relations.users_chores;
-			console.log(userChoreModels);
-			for (var j = 0; j < userChoreModels.length; j++){
-				
-				var userChore = userChoreModels.models[j].attributes;
-				userChores.push({
-					user_id: userChore.user_id,
-					order_index: userChore.order_index
-				});
-			}*/
-			new UserChoreCollection({chore_id: chore.id})
-			.fetch()
-			.then(function(ucModel){
-			console.log('Sanity');
-				for(var j = 0; j <ucModel.length; j++){
-					var userChore = ucModel.models[i].attributes;
-					userChores.push({
-						user_id: userChore.user_id,
-						order_index: userChore.oder_index
-					});
-				}
-			}).otherwise(function(){
-				res.json(503, {error: 'Database error'});
-			});
-			
-			chores.push({
-				id: chore.id,
-				name: chore.name,
-				createdate: chore.createdate,
-				duedate: chore.duedate,
-				interval: chore.interval,
-				completed: chore.completed,
-				reocurring_id: chore.reocurring_id,
-				user_id: chore.user_id,
-				apartment_id: chore.apartment_id,
-				userChores: userChores
-			});
-		}
-		res.json({chores: chores});
-	}).otherwise(function(){
-		res.json(503,{error: 'Database error'});
-	});
-  });
-
-  // Process chore form and adds to database
-  app.post('/chores', function(req, res) {
-	if (req.user === undefined) {
-		res.json(401, {error: 'Unauthorized user.'});
-		return;
-	}
-		var name = req.body.name;
-		var apartmentId = req.user.attributes.apartment_id;
-		var userId = req.user.attributes.id;
-		var duedate = req.body.duedate;
-		var date = new Date();
-		var createDate = (date.getMonth() + 1) + '/' + date.getDate()
-			+'/' + date.getFullYear();
-
-		var interval = req.body.interval;
-		var roommates = JSON.parse(req.body.roommates);
-	  
-		 if(!isValidName(name)){
-			res.json(400, {error: 'Invalid chore name.'});
-			return;
-		}
-		
-		// Need to check that date is valid ie on or after date created
-		
-		new ChoreModel({apartment_id: apartmentId,
-					name: name,
-					duedate: duedate,
-					createdate: createDate,
-					user_id: userId,
-					completed: false,
-					interval: interval,
-					})
-					.save()
-					.then(function(model){
-					for(var i = 0; i  < roommates.length; i++){
-						new UserChoreModel({
-							user_id: roommates[i].id,
-							chore_id: model.id,
-							order_index: i
-						})
-						.save()
-						.then(function(choremodel){
-						})
-						.otherwise(function(){
-							res.json(503, {error:'Database error'});
+			var lastChoreId = -1;
+			var name, dueDate, createDate, interval, completed;
+			for(var i = 0; i < rows.length; i++){
+				//If choreid is differnet, then all users_chores for the current
+				// chore have been pushed on users_chores. we push the chore then
+				if(rows[i].chore_id != lastChoreId){
+					if(lastChoreId !== -1){
+						chores.push({
+							id: lastChoreId,
+							name: name,
+							createDate: createDate,
+							dueDate: dueDate,
+							interval: interval,
+							completed: completed,
+							//reocurring_id: chore.reocurring_id,
+							//user_id: chore.user_id,
+							//apartment_id: chore.apartment_id,
+							users_chores: users_chores
 						});
-						
-						if(i === roommates.length-1){
-							res.send(201);
-						}
 					}
-					
-					}).otherwise(function(){
-					res.json(503,{error: 'DataBase error'});
+				//empty users_chores
+				users_chores = [];
+				lastChoreId = rows[i].chore_id;
+				name = rows[i].name;
+				createDate = rows[i].createdate;
+				dueDate = rows[i].duedate;
+				interval = rows[i].interval;
+				completed = rows[i].completed;
+				
+				}
+				
+				users_chores.push({
+					userId: rows[i].users_id,
+					name: rows[i].first_name,
+					order_index: rows[i].order_index
 				});
+			}
+				chores.push({
+					id: lastChoreId,
+					name: name,
+					createDate: createDate,
+					dueDate: dueDate,
+					interval: interval,
+					completed: completed,
+					users_chores: users_chores
+				});
+				res.json({chores: chores});
+		})
+		.otherwise(function(error){
+			console.log(error);
+			res.json(503, {error: 'Database error.'});
+		});
   });
-
-  // Get the chore information
- // Get the chore information
+  
+  
+// Get the chore information
   app.get('/chores/:chore', function(req, res) {
   	if (req.user === undefined) {
 		res.json(401, {error: 'Unauthorized user.'});
@@ -174,6 +135,66 @@ var chores = function(app) {
 	
   });
 
+  // Process chore form and adds to database
+  app.post('/chores', function(req, res) {
+	if (req.user === undefined) {
+		res.json(401, {error: 'Unauthorized user.'});
+		return;
+	}
+		var name = req.body.name;
+		var apartmentId = req.user.attributes.apartment_id;
+		var userId = req.user.attributes.id;
+		var duedate = req.body.duedate;
+		var date = new Date();
+		var createDate = (date.getMonth() + 1) + '/' + date.getDate()
+			+'/' + date.getFullYear();
+
+		var interval = req.body.interval;
+		
+		
+		var roommates = JSON.parse(req.body.roommates);
+		//var roommates = req.body.roommates;
+		if(!isValidName(name)){
+			res.json(400, {error: 'Invalid chore name.'});
+			return;
+		}
+		
+		// Need to check that date is valid ie on or after date created
+		
+		new ChoreModel({apartment_id: apartmentId,
+					name: name,
+					duedate: duedate,
+					createdate: createDate,
+					user_id: userId,
+					completed: false,
+					interval: interval,
+					})
+					.save()
+					.then(function(model){
+					for(var i = 0; i  < roommates.length; i++){
+						new UserChoreModel({
+							user_id: roommates[i].id,
+							chore_id: model.id,
+							order_index: i
+						})
+						.save()
+						.then(function(choremodel){
+						})
+						.otherwise(function(){
+							res.json(503, {error:'Database error'});
+						});
+						
+						if(i === roommates.length-1){
+							res.send(201);
+						}
+					}
+					
+					}).otherwise(function(){
+					res.json(503,{error: 'DataBase error'});
+				});
+  });
+
+ 
   // Update the chore
   app.put('/chores/:chore', function(req, res) {
 	if (req.user === undefined) {
