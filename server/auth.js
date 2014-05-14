586ebc278d1f75cb3a10d63c1a0c11526a4ce304
Aengus McMillin <aengusmcmillin@gmail.com>
2014-05-14 15:35:25 -0700

    Cleaned up config js to remove unneccessary stuff. Don't need separate environments

--- server/auth.js
+++ server/auth.js
@@ -3,18 +3,18 @@
 // expose our config directly to our application using module.exports
 module.exports = {
 
-	/* jslint maxlen: 130 */
+  /* jslint maxlen: 130 */
 
-	'facebookAuth' : {
-		'clientID' 		: process.env.FB_ID, // your App ID
-		'clientSecret' 	: process.env.FB_SECRET, // your App Secret
-		'callbackURL' 	: process.env.FB_CALLBACK
-	},
+  'facebookAuth' : {
+    'clientID'      : process.env.FB_ID, // your App ID
+    'clientSecret'  : process.env.FB_SECRET, // your App Secret
+    'callbackURL'   : process.env.FB_CALLBACK
+  },
 
-	'googleAuth' : {
-		'clientID' 		: process.env.GOOG_ID,
-		'clientSecret' 	: process.env.GOOG_SECRET,
-		'callbackURL' 	: process.env.GOOG_CALLBACK
-	}
+  'googleAuth' : {
+    'clientID'      : process.env.GOOG_ID,
+    'clientSecret'  : process.env.GOOG_SECRET,
+    'callbackURL'   : process.env.GOOG_CALLBACK
+  }
 
 };
