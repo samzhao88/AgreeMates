7ef83054767a56b4cdd3c14afa190de0e5e488c6
Amit Burstein <amit.burstein@gmail.com>
2014-05-23 16:01:54 -0700

    Refactor comments back end

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,4 +1,5 @@
 // Comments routes
+// jshint camelcase: false
 
 'use strict';
 
@@ -6,17 +7,21 @@ var MessageModel = require('../models/message').model;
 var CommentModel = require('../models/comment').model;
 var HistoryModel = require('../models/history').model;
 
-var comments = function(app) {
+var Comments = {
 
-  app.post('/messages/:message/comments', function(req, res) {
-    
-    // Check that user is authorized
-    if(req.user === undefined) {
+  // Sets up all routes
+  setup: function(app) {
+    app.post('/messages/:message/comments', Comments.addComment);
+    app.delete('/messages/:message/comments/:comment', Comments.deleteComment);
+  },
+
+  // Adds a comment to a message
+  addComment: function(req, res) {
+    if (req.user === undefined) {
       res.json(401, {error: 'Unauthorized user.'});
       return;
     }
-    
-    // Copy over fields from request
+
     var apartmentId = req.user.attributes.apartment_id;
     var userId = req.user.attributes.id;
     var messageId = req.params.message;
@@ -24,139 +29,115 @@ var comments = function(app) {
     var date = new Date();
 
     if (!isValidId(messageId)) {
-      res.json(400, {error: 'Invalid message id.'});
+      res.json(400, {error: 'Invalid message ID.'});
       return;
     } else if (text === null) {
       res.json(400, {error: 'Comment content cannot be null.'});
       return;
     }
 
-    new CommentModel({user_id: userId, message_id: messageId, 
-                     body: text, date: date})
-      .save()
-      .then(function(model) {
-        new MessageModel({id: messageId})
-          .fetch()
-          .then(function(message) {
+    Comments.createComment(userId, messageId, text, date,
+      function then(model) {
+        Comments.getMessage(messageId,
+          function then(message) {
             var historyString = req.user.attributes.first_name + ' ' +
-              req.user.attributes.last_name + ' added a comment to message "' +
-              message.attributes.subject.trim() + '" on the message board';
-            new HistoryModel({apartment_id: apartmentId,
-              history_string: historyString, date: new Date()})
-              .save()
-          });
-        model.author = req.user.attributes.first_name;
-        res.json(model);
-      }).otherwise(function(error) {
-        res.json(503, {error: 'Database error'});
-      });
-    
-  });
-
-  app.put('/messages/:message/comments/:comment', function(req, res) {
-    
-    // Check if user is authorized
-    if(req.user === undefined) {
-      res.json(401, {error: 'Unauthorized user.'});
-      return;
-    }
-
-    // Copy over the fields
-    var userId = req.user.attributes.id;
-    var messageId = req.params.message;
-    var commentId = req.params.comment;
-    var text = req.body.text;
-    var apartmentId = req.user.attributes.apartment_id;
-
-    if(!isValidId(messageId)) {
-      res.json(400, {error: 'Invalid message id.'});
-      return;
-    } else if(!isValidId(commentId)) {
-      res.json(400, {error: 'Invalid comment id.'});
-      return;
-    } else if(text === null) {
-      res.json(400, {error: 'Comment body cannot be null.'});
-      return;
-    }
-
-    new CommentModel({id: commentId, message_id: messageId, user_id: userId})
-      .save({body: text})
-      .then(function(model) {
-        new MessageModel({id: messageId})
-          .fetch()
-          .then(function(message) {
-            var historyString = req.user.attributes.first_name + ' ' +
-              req.user.attributes.last_name + 
-              ' edited their comment on the message  "' +
+              req.user.attributes.last_name + ' commented on message "' +
               message.attributes.subject.trim() + '"';
-            new HistoryModel({apartment_id: apartmentId,
-              history_string: historyString, date: new Date()})
-              .save()
-          });
-        res.json({id: model.attributes.id});
-      }).otherwise(function(error) {
-        res.json(503, {error: 'Database error.'});
-      });    
-  });
-
-  app.delete('/messages/:message/comments/:comment', function(req, res) {
-    // Check if user is authorized
-    if(req.user === undefined) {
-      res.json(401, {error: 'Unauthorized user.'});
-      return;
-    }
- 
-    var userId = req.user.attributes.id;
-    var messageId = req.params.message;
-    var commentId = req.params.comment;
-    var apartmentId = req.user.attributes.apartment_id;
-
-    // Check if fields are valid  
-    if(!isValidId(messageId)) {
-      res.json(400, {error: 'Invalid message id.'});
-      return;
-    } else if(!isValidId(commentId)) {
-      res.json(400, {error: 'Invalid comment id.'});
-      return;
-    }
-
-    new MessageModel({id: messageId})
-      .fetch()
-      .then(function(model) {
-        var historyString = req.user.attributes.first_name + ' ' +
-          req.user.attributes.last_name + 
-          ' deleted their comment on the message "' +
-          model.attributes.subject.trim() + '"';
-        new HistoryModel({apartment_id: apartmentId,
-          history_string: historyString, date: new Date()})
-          .save()
-
-        // Get the comment, make sure the user_id is the user
-        // trying to delete it.
-        new CommentModel()
-          .query('where', 'message_id', '=', messageId, 'AND',
-                 'id', '=', commentId, 'AND',
-                 'user_id', '=', userId)
-          .destroy()
-          .then(function() {
-            res.send(200);
-          }).otherwise(function(error) {
+            Comments.createHistory(apartmentId, historyString);
+          },
+          function otherwise() {
             res.json(503, {error: 'Database error.'});
           });
-      }).otherwise(function(error) {
+
+        model.author = req.user.attributes.first_name;
+        res.json(model);
+      },
+      function otherwise(error) {
+        console.log(error);
         res.json(503, {error: 'Database error.'});
       });
-  });
+  },
 
-  // Checks if a value is an integer
-  function isInt(value) {
-    return !isNaN(value) && parseInt(value) == value;
+  // Adds a comment to a message in the DB
+  createComment: function(userId, messageId, text, date, thenFun, otherFun) {
+    new CommentModel({user_id: userId, message_id: messageId, body: text, date: date})
+      .save()
+      .then(thenFun)
+      .otherwise(otherFun);
+  },
+
+  // Gets a message from the DB
+  getMessage: function(messageId, thenFun, otherFun) {
+    new MessageModel({id: messageId})
+      .fetch()
+      .then(thenFun)
+      .otherwise(otherFun);
+  },
+
+  // Creates a history item
+  createHistory: function(apartmentId, historyString) {
+    new HistoryModel({apartment_id: apartmentId, history_string: historyString, date: new Date()})
+    .save();
+  },
+
+  // Deletes a comment on a message
+  deleteComment: function(req, res) {
+    if (req.user === undefined) {
+      res.json(401, {error: 'Unauthorized user.'});
+      return;
+    }
+
+    var userId = req.user.attributes.id;
+    var messageId = req.params.message;
+    var commentId = req.params.comment;
+    var apartmentId = req.user.attributes.apartment_id;
+
+    if (!isValidId(messageId)) {
+      res.json(400, {error: 'Invalid message ID.'});
+      return;
+    } else if (!isValidId(commentId)) {
+      res.json(400, {error: 'Invalid comment ID.'});
+      return;
+    }
+
+    Comments.getMessage(messageId,
+      function then(model) {
+        var historyString = req.user.attributes.first_name + ' ' +
+          req.user.attributes.last_name + ' deleted their comment on message "' +
+          model.attributes.subject.trim() + '"';
+        Comments.createHistory(apartmentId, historyString);
+        Comments.destroyComment(messageId, commentId, userId,
+          function then(model) {
+            res.send(200);
+          },
+          function otherwise() {
+            res.json(503, {error: 'Database error.'});
+          });
+      },
+      function otherwise() {
+        res.json(503, {error: 'Database error.'});
+      });
+  },
+
+  // Deletes a comment on a message in the DB
+  destroyComment: function(messageId, commentId, userId, thenFun, otherFun) {
+    new CommentModel()
+      .query('where', 'id', '=', commentId, 'AND', 'message_id', '=', messageId, 'AND', 'user_id', '=', userId)
+      .destroy()
+      .then(thenFun)
+      .otherwise(otherFun);
   }
 
-  // Checks if a bill ID is valid
-  function isValidId(id) {
-    return isInt(id) && id > 0;
-  }
 };
 
-module.exports = comments;
+// Checks if a value is an integer
+function isInt(value) {
+  return !isNaN(value) && parseInt(value) == value;
+}
+
+// Checks if a bill ID is valid
+function isValidId(id) {
+  return isInt(id) && id > 0;
+}
+
+module.exports = Comments;
