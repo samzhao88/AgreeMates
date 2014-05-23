6bfe8a72a18cb6a4ab70589b77d8363bcb6ebca6
Amit Burstein <amit.burstein@gmail.com>
2014-05-22 22:55:43 -0700

    Remove profile page from sidebar

--- public/test/test.spec.js
+++
@@ -1,6 +0,0 @@
-// This file is test code for front end.
-describe("A test suite", function() {
-	it ('should fail before actual tests have been added', function() {
-		expect(true).to.be.true;
-	});
-});
